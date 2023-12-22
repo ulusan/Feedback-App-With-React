8aa688753f91c1451837ad3b6db8324d0ee155ee
Kerimsekili <kerimsekilix@gmail.com>
2023-12-22 18:44:46 +0300

    Creating a Component & Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="container">
-      <h1>My App</h1>
-      <p>Hello</p>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
