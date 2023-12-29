c787b0f8383714334a08e207af391ac7edbc5f23
Kerimsekili <kerimsekilix@gmail.com>
2023-12-29 22:35:45 +0300

    Adding React Routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import Feedbacklist from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -20,14 +22,20 @@ function App() {
     }
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <Feedbacklist feedback={feedback} handleDelete={deletFeedback} />
+        <Route exact path="/">
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback} />
+          <Feedbacklist feedback={feedback} handleDelete={deletFeedback} />
+        </Route>
+
+        <Routes>
+          <Route path="/about" component={AboutPage} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
