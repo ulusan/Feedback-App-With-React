f12089cd74fc27cd1f9948456d6352e3520ae054
Kerimsekili <kerimsekilix@gmail.com>
2024-01-02 16:16:23 +0300

    Moving Functions to Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { v4 as uuidv4 } from "uuid";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import Header from "./components/Header";
 import Feedbacklist from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
@@ -11,18 +8,6 @@ import { FeedbackProvider } from "./context/FeedbackContext";
 import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
-  const deletFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete ?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
   return (
     <FeedbackProvider>
       <Router>
@@ -34,12 +19,9 @@ function App() {
               path="/"
               element={
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
-                  <FeedbackStats feedback={feedback} />
-                  <Feedbacklist
-                    feedback={feedback}
-                    handleDelete={deletFeedback}
-                  />
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <Feedbacklist />
                 </>
               }
             ></Route>
